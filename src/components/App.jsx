4bc3453f190a1c646ac66ab75a932c9ad3753807
OlenaPatlata <94738131+OlenaPatlata@users.noreply.github.com>
2022-04-27 11:15:20 +0300

    add useRef

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,16 @@
-import React, { Component } from 'react';
-// import { get, save, remove } from 'components/services/localStorage';
+import React, { useState, useEffect, useRef } from 'react';
 import ContactsList from 'components/ContactsList/ContactsList';
 import Form from 'components/Form/Form';
 import Filter from 'components/Filter/Filter';
 import shortid from 'shortid';
 import s from './App.module.css';
 
-class App extends Component {
-  state = {
-    contacts: [],
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filterWords, setFilterWords] = useState('');
+  const isFirstRender = useRef(true);
 
-    filter: '',
-  };
-
-  save = (key, value) => {
+  const save = (key, value) => {
     try {
       localStorage.setItem(key, JSON.stringify(value));
     } catch (error) {
@@ -21,7 +18,7 @@ class App extends Component {
     }
   };
 
-  get = key => {
+  const get = key => {
     try {
       return JSON.parse(localStorage.getItem(key));
     } catch (error) {
@@ -29,20 +26,24 @@ class App extends Component {
     }
   };
 
-  componentDidMount() {
-    const parsedContacts = this.get('contacts');
+  useEffect(() => {
+    const parsedContacts = get('contacts');
     if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
+      setContacts(parsedContacts);
     }
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState !== this.state.contacts) {
-      this.save('contacts', this.state.contacts);
-    }
-  }
+  }, []);
 
-  formSubmitHandler = data => {
-    const { contacts } = this.state;
+  useEffect(() => {
+    if (isFirstRender.current) {
+      console.log(isFirstRender.current);
+      isFirstRender.current = false;
+      return;
+    }
+    console.log(isFirstRender.current);
+    save('contacts', contacts);
+  }, [contacts]);
+
+  const formSubmitHandler = data => {
     const normalizedName = data.name
       .toLocaleLowerCase()
       .split(' ')
@@ -64,59 +65,156 @@ class App extends Component {
       );
       return;
     }
-    this.addContact(data);
+    addContact(data);
   };
 
-  addContact = contactNew => {
+  const addContact = contactNew => {
     const contactAdd = {
       id: shortid.generate(),
       name: contactNew.name,
       number: contactNew.number,
     };
-    this.setState(prevState => ({
-      contacts: [contactAdd, ...prevState.contacts],
-    }));
+    setContacts(prev => [...prev, contactAdd]);
   };
 
-  deleteContact = e => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(
-        contact => contact.id !== e.target.id
-      ),
-    }));
+  const deleteContact = e => {
+    const smolllList = contacts.filter(contact => contact.id !== e.target.id);
+    setContacts(smolllList);
   };
 
-  changeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
-  };
-
-  getVisibleContacts = () => {
-    const { contacts, filter } = this.state;
-    if (filter) {
-      const normalizedFilter = filter.toLocaleLowerCase().trim();
-      return contacts.filter(contact =>
-        contact.name.toLocaleLowerCase().includes(normalizedFilter)
-      );
-    }
-    return contacts;
-  };
-
-  render() {
-    const { filter } = this.state;
-    const vizibleContacts = this.getVisibleContacts();
-
-    return (
-      <div className={s.wrapper}>
-        <h1 className={s.title}>Phonebook</h1>
-        <Form onSubmit={this.formSubmitHandler} />
-        <h1 className={s.title}>Contacts</h1>
-        <Filter value={filter} onChangeFilter={this.changeFilter} />
-        <ContactsList
-          vizibleContacts={vizibleContacts}
-          onDeleteContact={this.deleteContact}
-        />
-      </div>
-    );
+  function changeFilter(e) {
+    return (() => setFilterWords(e.currentTarget.value))();
   }
-}
+
+  const getVisibleContacts = () => {
+    if (!filterWords) {
+      return;
+    }
+    const normWord = filterWords.toLocaleLowerCase().trim();
+    const findContact = [...contacts].filter(contact =>
+      contact.name.toLocaleLowerCase().includes(normWord)
+    );
+    return findContact;
+  };
+
+  const vizibleContacts = getVisibleContacts();
+
+  return (
+    <div className={s.wrapper}>
+      <h1 className={s.title}>Phonebook</h1>
+      <Form onSubmit={formSubmitHandler} />
+      <h1 className={s.title}>Contacts</h1>
+      <Filter value={filterWords} onChangeFilter={changeFilter} />
+      <ContactsList
+        vizibleContacts={vizibleContacts || contacts}
+        onDeleteContact={deleteContact}
+      />
+    </div>
+  );
+};
 export default App;
+
+// ___________________________Example useState_________
+// const App = () => {
+//   // const [counter, setCounter] = useState(0);
+//   const [counter, setCounter] = useState(() => randomCounter());
+//   const [state, setState] = useState({ title: 'Counter', date: new Date() });
+
+//   function increment() {
+//     setCounter(prev => prev + 1);
+//   }
+//   function decrement() {
+//     setCounter(prev => prev - 1);
+//   }
+//   function randomCounter() {
+//     console.log('++++++++');
+//     return Math.trunc(Math.random() * 20);
+//   }
+//   function updateTitle() {
+//     setState(prev => {
+//       return { ...prev, title: 'newTitle' };
+//     });
+//   }
+
+//   return (
+//     <div>
+//       <h1>Счетчик{counter}</h1>
+//       <p></p>
+//       <button type="button" onClick={increment}>
+//         Добавить
+//       </button>
+//       <button type="button" onClick={decrement}>
+//         Убрать
+//       </button>
+//       <button type="button" onClick={updateTitle}>
+//         Change
+//       </button>
+//       <pre>{JSON.stringify(state, null, 2)}</pre>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// ___________________________Example useEffect_________
+
+// const App = () => {
+//   const [type, setType] = useState('users');
+//   const [data, setData] = useState([]);
+//   const [pos, setPos] = useState({ x: 0, y: 0 });
+//   const changeUsers = () => {
+//     setType('users');
+//   };
+//   const changeTodos = () => {
+//     setType('Todu');
+//   };
+//   const changePost = () => {
+//     setType('Пости');
+//   };
+
+//   const changeTitle = e => {
+//     setType(e.target.name);
+//   };
+//   const mouseMoveHandler = event => {
+//     setPos({ x: event.clientX, y: event.clientY });
+//   };
+//   useEffect(() => {
+//     fetch(`https://jsonplaceholder.typicode.com/${type}`)
+//       .then(response => response.json())
+//       .then(json => setData(json));
+//   }, [type]);
+
+//   useEffect(() => {
+//     console.log('ComponentDidMount');
+//     window.addEventListener('mousemove', mouseMoveHandler);
+
+//     return window.removeEventListener('mousemove', mouseMoveHandler);
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Ресурс: {type}</h1>
+//       <button name="users" type="button" onClick={changeTitle}>
+//         Користувачі
+//       </button>
+//       <button name="todos" type="button" onClick={changeTitle}>
+//         Todos
+//       </button>
+//       <button name="posts" type="button" onClick={changeTitle}>
+//         Пости
+//       </button>
+//       <pre>{JSON.stringify(pos, null, 2)}</pre>;
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// ___________________________Example useContext_________
+
+// const App = () => {
+//   const [value, setValue] = useRef('');
+//   return <div></div>;
+// };
+
+// export default App;
